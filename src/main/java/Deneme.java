985f9219342cd821b76af5e4db01d7005748d5c6
ismail özer <makikohoca@gmail.com>
2023-02-25 19:28:34 +0300

    v3

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -2,5 +2,6 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("merhaba");
         System.out.println("v2 için merhaba");
+        System.out.println("mggmff");
     }
 }
