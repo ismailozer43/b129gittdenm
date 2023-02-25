ecdca673aafb4cbc95089e20721db53236203674
ismailozer43 <121877876+ismailozer43@users.noreply.github.com>
2023-02-25 19:30:33 +0300

    Update Deneme.java

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -3,5 +3,6 @@ public class Deneme {
         System.out.println("merhaba");
         System.out.println("v2 için merhaba");
         System.out.println("mggmff");
+        //github tarafından yolluyorum
     }
 }
